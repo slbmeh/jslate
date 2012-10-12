f3bcfaaa919c2085b98a1ca0757ff615d22c6dd2
Andrej Zieger <azieger@ubuntu.versant.com>
2012-10-12 13:26:08 +0200

    Update to Cake 2.1 + Login restored... most other broken

---
+++ app/Config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(    
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'jslate',
+		'password' => 'jslate',
+		'database' => 'jslate',
+		'prefix' => '',
+	);
+}
+?>
